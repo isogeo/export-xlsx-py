4e3bd85b0219674e5b08f967294f9d14ad744ee3
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2020-03-02 12:05:34 +0100

    Clean package __init__.py
    #40

--- isogeotoxlsx/__init__.py
+++ isogeotoxlsx/__init__.py
@@ -8,7 +8,7 @@
 # submodules
 from .__about__ import __version__  # noqa: F401
 from .isogeo2xlsx import Isogeo2xlsx  # noqa: F401
-from .isogeofromxlsx import *  # noqa: F401
+from .isogeoFromxlsx import IsogeoFromxlsx, dict_inspire_fr, dict_inspire_en  # noqa: F401
 
 # subpackages
 from .matrix import *  # noqa: F401,F403
